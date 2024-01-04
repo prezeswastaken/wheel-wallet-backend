4cf455563f98c23ecbb4ce1982b8277a17421efc
DominikSk0rka <Sk0rka.dev@gmail.com>
2024-01-04 15:55:18 +0100

    Added Failed_jobsTest

---
+++ tests/Unit/MigrationsTests/Failed_jobsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
+
+class Failed_jobsTest extends TestCase
+{
+    use RefreshDatabase;
+
+   
+    public function test_cars_table_has_expected_columns()
+    {
+       
+        $this->artisan('migrate');
+
+        $this->assertTrue(Schema::hasTable('failed_jobs'));
+
+        $this->assertTrue(Schema::hasColumns('failed_jobs', [
+            'id',
+            'uuid',
+            'connection',
+            'queue',
+            'payload',
+            'exception',
+            'failed_at',
+        ]));
+    }
+
+   
+}
