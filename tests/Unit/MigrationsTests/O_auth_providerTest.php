8f533c9b7147bd522d9a47cf1f0e901c87c923e6
DominikSk0rka <Sk0rka.dev@gmail.com>
2024-01-04 16:01:39 +0100

    Added O_auth_providerTest

---
+++ tests/Unit/MigrationsTests/O_auth_providerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
+
+class O_auth_providerTest extends TestCase
+{
+    use RefreshDatabase;
+
+   
+    public function test_cars_table_has_expected_columns()
+    {
+       
+        $this->artisan('migrate');
+
+        $this->assertTrue(Schema::hasTable('o_auth_providers'));
+
+        $this->assertTrue(Schema::hasColumns('o_auth_providers', [
+            'id',
+            'provider',
+            'provider_id',
+            'created_at',
+            'updated_at',
+            'user_id'
+        ]));
+    }
+
+   
+}
